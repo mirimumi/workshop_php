f74cadfd06c5892a141631cd65fd1480618bb2c7
mirimumi <0931961754zxc@gmail.com>
2022-01-11 11:37:40 +0700

    V.1

--- layout/header.php
+++ layout/header.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>:: ระบบบริหารจัดการสมาชิกที่น่ารัก::</title>
+  <title>:: ระบบบริหารจัดการสมาชิกที่ ::</title>
 
   <!-- Google Font -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
